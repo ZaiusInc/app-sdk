f9b4f1c575b07aaff67b426fc42f0f7111f375ad
mejmt <john@zaius.com>
2019-06-03 12:30:40 -0400

    fixing example project lifecycle implementation

--- example-project/src/lifecycle/Lifecycle.ts
+++ example-project/src/lifecycle/Lifecycle.ts
@@ -1,23 +1,21 @@
 import * as Zap from '@zaius/zap';
-import {Response} from '../../../src/zap/lib';
-import {LifecycleResult} from '../../../src/zap/types';
 
 export class Lifecycle extends Zap.Lifecycle {
-  public async onSetupForm(_page: string, _action: string, _formData: object): Promise<Response> {
-    const response = new Response();
+  public async onSetupForm(_page: string, _action: string, _formData: object): Promise<Zap.Response> {
+    const response = new Zap.Response();
     response.status = 200;
     return Promise.resolve(response);
   }
 
-  public async onUpgrade(_fromVersion: string): Promise<LifecycleResult> {
+  public async onUpgrade(_fromVersion: string): Promise<Zap.LifecycleResult> {
     return Promise.resolve({success: true});
   }
 
-  public async onFinalizeUpgrade(_fromVersion: string): Promise<LifecycleResult> {
+  public async onFinalizeUpgrade(_fromVersion: string): Promise<Zap.LifecycleResult> {
     return Promise.resolve({success: true});
   }
 
-  public async onUninstall(): Promise<LifecycleResult> {
+  public async onUninstall(): Promise<Zap.LifecycleResult> {
     return Promise.resolve({success: true});
   }
 }
