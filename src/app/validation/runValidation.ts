eef7f319c23deeda65fc71e760d5eb8ad8e9e11b
mejmt <john@zaius.com>
2019-09-14 00:22:13 -0400

    change validation script to use friendly output and proper exit code

--- src/app/validation/runValidation.ts
+++ src/app/validation/runValidation.ts
@@ -1,9 +1,20 @@
+import chalk from 'chalk';
 import * as path from 'path';
 import {Runtime} from '../Runtime';
 import {validateApp} from './validateApp';
 
 Runtime.initialize(path.resolve(process.cwd(), 'dist'), true).then(async (runtime) => {
-  console.log(JSON.stringify({errors: await validateApp(runtime)}));
+  try {
+    const errors = await validateApp(runtime);
+    if (errors.length > 0) {
+      console.error(chalk.red(`Validation failed:\n${errors.map((e) => ` * ${e}`).join('\n')}`));
+      process.exit(1);
+    } else {
+      console.log(chalk.green(`Looks good to me`));
+    }
+  } catch (e) {
+    console.error(chalk.red(`Validation process failed: ${e.message}`));
+  }
 }).catch((e) => {
-  console.error(`Validation process failed: ${e.message}`);
+  console.error(chalk.red(`Validation process failed: ${e.message}`));
 });
