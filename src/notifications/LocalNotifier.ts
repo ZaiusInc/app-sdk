91f2233c3719fb361a98863c1f74968e2b3651fd
Sean McDaniel <sean.mcdaniel@me.com>
2019-09-19 13:30:37 -0400

    ZAIOUS-10029: AcvitityLogger => Notifier

--- src/notifications/LocalNotifier.ts
+++ src/notifications/LocalNotifier.ts
@@ -1,12 +1,12 @@
-import {ActivityLogger} from './ActivityLogger';
+import {Notifier} from './Notifier';
 
 // tslint:disable-next-line:only-arrow-functions
 const noop = function() { /**/ };
 
 /**
- * A simple noop stub of the activity api
+ * A simple noop stub of the notifier api
  */
-export class LocalActivityLogger implements ActivityLogger {
+export class LocalNotifier implements Notifier {
   public info(_title: string, _activity: string, _summary: string, _details: string) {
     noop();
   }
---
+++ src/notifications/notifications.test.ts
@@ -0,0 +1,29 @@
+import 'jest';
+import {Notifier, setNotifier, notifications} from '../notifications';
+
+describe('activityLog', () => {
+  describe('initialize', () => {
+    it('replaces the local logger with the provided logger', () => {
+      const mockNotifier: Notifier = {
+        info: jest.fn(),
+        success: jest.fn(),
+        warn: jest.fn(),
+        error: jest.fn()
+      };
+
+      setNotifier(mockNotifier);
+
+      notifications.info('activity', 'title', 'summary', 'detail');
+      expect(mockNotifier.info).toHaveBeenCalled();
+
+      notifications.success('activity', 'title', 'summary', 'detail');
+      expect(mockNotifier.success).toHaveBeenCalled();
+
+      notifications.warn('activity', 'title', 'summary', 'detail');
+      expect(mockNotifier.warn).toHaveBeenCalled();
+
+      notifications.error('activity', 'title', 'summary', 'detail');
+      expect(mockNotifier.error).toHaveBeenCalled();
+    });
+  });
+});
