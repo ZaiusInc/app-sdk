b30c3f4144c313a6d1d519db4c1d6b7b6ce26fba
John Thomas <johnlives@gmail.com>
2019-09-27 15:38:09 -0400

    ZAIUS-10080: adding increment methods for kv store interface (and exists to base) (#25)

--- jest.config.js
+++ jest.config.js
@@ -15,7 +15,18 @@ module.exports = {
   setupFilesAfterEnv: ['./src/test/setup.ts'],
   verbose: true,
   // note: jobApi.ts is presently only interfaces. see: https://github.com/kulshekhar/ts-jest/issues/378
-  collectCoverageFrom: ['src/**/*.ts', '!src/**.index.ts', '!src/test/**/*', '!src/jobs/JobApi.ts'],
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/index.ts',
+    '!src/test/**/*',
+    '!src/functions/FunctionApi.ts',
+    '!src/functions/LocalFunctionApi.ts',
+    '!src/jobs/JobApi.ts',
+    '!src/jobs/LocalJobApi.ts',
+    '!src/notifications/LocalNotifier.ts',
+    '!src/store/LocalStore.ts',
+    '!src/store/LocalKVStore.ts',
+  ],
   coverageThreshold: {
     global: {
       branches: 90,
