83165d2bac26e9ff83d8aacdc7dd6c407cd5a7f1
mejmt <john@zaius.com>
2019-10-15 10:26:03 -0400

    options are... optional. indeed.

--- src/app/types/AppManifest.ts
+++ src/app/types/AppManifest.ts
@@ -49,8 +49,8 @@ export interface AppManifest {
   channel?: {
     grouping: string;
     targeting: 'dynamic' | CampaignTargeting[];
-    options: {
-      prepare: boolean;
+    options?: {
+      prepare?: boolean;
     }
   };
 }
