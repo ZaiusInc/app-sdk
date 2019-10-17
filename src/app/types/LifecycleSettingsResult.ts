50b2a716e1e36438418806579a32b9dec133b441
Levi Lansing <helives@gmail.com>
2019-10-16 22:55:53 -0400

    more documentation

--- src/app/types/LifecycleSettingsResult.ts
+++ src/app/types/LifecycleSettingsResult.ts
@@ -1,21 +1,41 @@
-export type Intent = 'info' | 'error' | 'success' | 'danger';
+export type Intent = 'info' | 'success' | 'warning' | 'danger';
 
+/**
+ * @hidden
+ */
 export interface LifecycleSettingsResponse {
   success: boolean;
   errors?: {[ref: string]: string[]};
   toasts?: Array<{intent: Intent, message: string}>;
 }
 
+/**
+ * Used to compose a response to the onSettingsForm lifecycle request
+ */
 export class LifecycleSettingsResult {
   private errors: {[ref: string]: string[]} = {};
   private toasts: Array<{intent: Intent, message: string}> = [];
 
+  constructor(private page: string) {
+  }
+
+  /**
+   * Display a toast to user, such as, "Successfully authenticated with <Integration>" or
+   * "Authentication failed, please check your credentials and try again."
+   * @param intent One of the supported intents that will affect how the toast is displayed.
+   * @param message The string message to display in the toast
+   */
   public addToast(intent: Intent, message: string) {
     this.toasts.push({intent, message});
   }
 
-  public addError(section: string, field: string, error: string) {
-    const ref = `${section}.${field}`;
+  /**
+   * Add an error to display to the user for a particular form field
+   * @param field The field key to display the error under
+   * @param error The error message to display to the user
+   */
+  public addError(field: string, error: string) {
+    const ref = `${this.page}.${field}`;
     if (!this.errors[ref]) {
       this.errors[ref] = [error];
     } else {
@@ -23,10 +43,18 @@ export class LifecycleSettingsResult {
     }
   }
 
+  /**
+   * @hidden
+   * Used internally to get a proper status code for the result
+   */
   public statusCode() {
     return Object.keys(this.errors).length === 0 ? 200 : 400;
   }
 
+  /**
+   * @hidden
+   * Used internally to get the response body
+   */
   public response(): LifecycleSettingsResponse {
     if (Object.keys(this.errors).length === 0) {
       return {success: true};
