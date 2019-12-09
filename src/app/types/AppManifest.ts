3dee8837464379e49c97a4c6df044b79e8759ac2
mejmt <john@zaius.com>
2019-12-09 16:59:45 -0500

    add description to input definition

--- src/app/types/AppManifest.ts
+++ src/app/types/AppManifest.ts
@@ -23,6 +23,7 @@ export interface AppLiquidExtension {
     [name: string]: {
       type: 'string' | 'number' | 'boolean' | 'any';
       required: boolean;
+      description: string;
     };
   };
 }
