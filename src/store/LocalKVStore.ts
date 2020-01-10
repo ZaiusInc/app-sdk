eca45638530cdb891cb530d6ab5ea809bda200f6
Levi Lansing <helives@gmail.com>
2020-01-10 16:25:17 -0500

    ZAIUS-9872: enhanced Job interface to support interruption - partial implementation of local kv store for testing purposes

--- src/store/LocalKVStore.ts
+++ src/store/LocalKVStore.ts
@@ -1,7 +1,23 @@
+import {logger} from '../logging';
 import {Value} from './BaseKVStore';
 import {KVHash, KVPatchUpdater, KVRowOptions, KVStore, MultiValue} from './KVStore';
 import {NumberSet} from './NumberSet';
 import {StringSet} from './StringSet';
+
+let kvStoreData: {[key: string]: any} = {};
+
+export function resetLocalKVStore() {
+  kvStoreData = {};
+}
+
+function filterFields(result: any, fields?: string[]) {
+  if (fields && fields.length > 0) {
+    const filtered: any = {};
+    fields.forEach((f) => filtered[f] = result[f]);
+    return filtered;
+  }
+  return result;
+}
 
 /**
  * @hidden
@@ -10,26 +26,44 @@ import {StringSet} from './StringSet';
  * @TODO implement the stub for local development purposes
  */
 export class LocalKVStore implements KVStore {
-  public get<T extends KVHash>(_key: string, _fields?: string[]): Promise<T> {
-    throw new Error('Method not implemented.');
+  public async get<T extends KVHash>(key: string, fields?: string[]): Promise<T> {
+    const result = kvStoreData[key] || {};
+    return filterFields(result, fields);
   }
 
-  public put<T extends KVHash>(_key: string, _value?: KVHash, _options?: KVRowOptions): Promise<T> {
-    throw new Error('Method not implemented.');
+  public async put<T extends KVHash>(key: string, value: T, options?: KVRowOptions): Promise<T> {
+    if (options?.ttl) {
+      logger.debug('ttl will be ignored in local KV store');
+    }
+    return (kvStoreData[key] = value) || {};
   }
 
-  public patch<T extends KVHash>(
-    _key: string, _value?: T | KVPatchUpdater<T>, _options?: KVRowOptions
+  public async patch<T extends KVHash>(
+    key: string, value: T | KVPatchUpdater<T>, options?: KVRowOptions
   ): Promise<T> {
-    throw new Error('Method not implemented.');
+    if (options?.ttl) {
+      logger.debug('ttl will be ignored in local KV store');
+    }
+    if (typeof value === 'function') {
+      return (kvStoreData[key] = value(kvStoreData[key], options || {})) || {};
+    } else {
+      return (kvStoreData[key] = Object.assign({}, kvStoreData[key], value));
+    }
   }
 
-  public delete<T extends KVHash>(_key: string, _fields?: string[]): Promise<T> {
-    throw new Error('Method not implemented.');
+  public async delete<T extends KVHash>(key: string, fields?: string[]): Promise<T> {
+    if (fields) {
+      const value = kvStoreData[key] || {};
+      fields.forEach((f) => delete value[f]);
+      kvStoreData[key] = value;
+    } else {
+      delete kvStoreData[key];
+    }
+    return kvStoreData[key] || {};
   }
 
-  public exists(_key: string): Promise<boolean> {
-    throw new Error('Method not implemented.');
+  public async exists(key: string): Promise<boolean> {
+    return Object.keys(kvStoreData).includes(key) && Object.keys(kvStoreData[key]).length > 0;
   }
 
   public increment(_key: string, _field: string, _amount?: number): Promise<number> {
