b545c89cee0a720896475ad3d6966e6b7d84eedb
Jason Aliyetti <jaliyetti@zaius.com>
2020-01-30 15:36:15 -0500

    ZAIUS-11113 Validate notifications (#50)

--- src/notifications/Notifier.ts
+++ src/notifications/Notifier.ts
@@ -1,36 +1,36 @@
 export interface Notifier {
   /**
    * Create an informational notification.
-   * @param activity The activity
-   * @param title The title
-   * @param summary The activity summary
+   * @param activity The activity, must not be empty
+   * @param title The title, must not be empty
+   * @param summary The activity summary, must not be empty
    * @param [details] The activity details
    */
   info(activity: string, title: string, summary: string, details?: string): void;
 
   /**
    * Create a success notification.
-   * @param activity The activity
-   * @param title The title
-   * @param summary The activity summary
+   * @param activity The activity, must not be empty
+   * @param title The title, must not be empty
+   * @param summary The activity summary, must not be empty
    * @param [details] The activity details
    */
   success(activity: string, title: string, summary: string, details?: string): void;
 
   /**
    * Create a warning notification.
-   * @param activity The activity
-   * @param title The title
-   * @param summary The activity summary
+   * @param activity The activity, must not be empty
+   * @param title The title, must not be empty
+   * @param summary The activity summary, must not be empty
    * @param [details] The activity details
    */
   warn(activity: string, title: string, summary: string, details?: string): void;
 
   /**
    * Create an error notification.
-   * @param activity The activity
-   * @param title The title
-   * @param summary The activity summary
+   * @param activity The activity, must not be empty
+   * @param title The title, must not be empty
+   * @param summary The activity summary, must not be empty
    * @param [details] The activity details
    */
   error(activity: string, title: string, summary: string, details?: string): void;
--- src/notifications/notifications.ts
+++ src/notifications/notifications.ts
@@ -1,13 +1,33 @@
 import {Notifier} from './Notifier';
 import {LocalNotifier} from './LocalNotifier';
+import {logger} from '../logging';
 
 let notifier: Notifier = new LocalNotifier();
 
+const validate = (activity: string, title: string, summary: string, _details?: string) => {
+  const errors = [];
+
+  if (activity.trim().length === 0) {
+    errors.push('activity cannot be blank');
+  }
+  if (title.trim().length === 0) {
+    errors.push('title cannot be blank');
+  }
+  if (summary.trim().length === 0) {
+    errors.push('summary cannot be blank');
+  }
+  if (errors.length === 0) {
+    return true;
+  } else {
+    logger.error(`Unable to send notification: ${errors.join(', ')}`);
+    return false;
+  }
+};
 /**
  * @hidden
  */
-export const setNotifier = (logger: Notifier) => {
-  notifier = logger;
+export const setNotifier = (otherNotifier: Notifier) => {
+  notifier = otherNotifier;
 };
 
 /**
@@ -15,18 +35,26 @@ export const setNotifier = (logger: Notifier) => {
  */
 export const notifications: LocalNotifier = {
   info(activity: string, title: string, summary: string, details?: string) {
-    notifier.info(activity, title, summary, details);
+    if (validate(activity, title, summary, details)) {
+      notifier.info(activity, title, summary, details);
+    }
   },
 
   success(activity: string, title: string, summary: string, details?: string) {
-    notifier.success(activity, title, summary, details);
+    if (validate(activity, title, summary, details)) {
+      notifier.success(activity, title, summary, details);
+    }
   },
 
   warn(activity: string, title: string, summary: string, details?: string) {
-    notifier.warn(activity, title, summary, details);
+    if (validate(activity, title, summary, details)) {
+      notifier.warn(activity, title, summary, details);
+    }
   },
 
   error(activity: string, title: string, summary: string, details?: string) {
-    notifier.error(activity, title, summary, details);
+    if (validate(activity, title, summary, details)) {
+      notifier.error(activity, title, summary, details);
+    }
   }
 };
