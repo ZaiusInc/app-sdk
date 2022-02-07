32d99e0104bebc4dc1407321f0917c467b181786
Mal Graty <mal.graty@googlemail.com>
2022-02-07 12:02:34 +0000

    Adding hidden fullpath

---
+++ src/app/lib/InternalRequest.ts
@@ -0,0 +1,19 @@
+import {HttpMethod} from '../types';
+import {QueryParams} from './QueryParams';
+import {QueryParams} from './Request';
+
+/**
+ * @hidden
+ */
+export class InternalRequest extends Request {
+  public readonly fullpath: string;
+
+  public constructor(
+    method: HttpMethod, fullpath: string, path: string, params: QueryParams,
+    headers: string[][], body: Uint8Array | null
+  ) {
+  {
+    super(method, path, params, headers, body);
+    this.fullpath = fullpath;
+  }
+}
