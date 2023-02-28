ffd55f224e1477f0fdd289a464c606556d85e0a7
Sean McDaniel <sean.mcdaniel@me.com>
2023-02-28 18:29:24 -0500

    OCP-195: export the default log level for anduin to use when configuring the sdks in preparation for a fn call or job (#100)

--- src/logging/Logger.ts
+++ src/logging/Logger.ts
@@ -103,10 +103,10 @@ let level: LogLevel;
 /**
  * @hidden
  * Set the current LogLevel
- * @param logLevel to apply, null value will set the level to the default
+ * @param logLevel to set
  */
 export function setLogLevel(logLevel: LogLevel) {
-  level = logLevel || DEFAULT_LOG_LEVEL;
+  level = logLevel;
 }
 
 /**
@@ -182,7 +182,7 @@ export interface ILogger {
   error(visibility: LogVisibility, ...args: any[]): void;
 }
 
-const DEFAULT_LOG_LEVEL = LOG_LEVELS_BY_STRING[process.env.LOG_LEVEL || 'debug'] || LogLevel.Debug;
+export const DEFAULT_LOG_LEVEL = LOG_LEVELS_BY_STRING[process.env.LOG_LEVEL || 'debug'] || LogLevel.Debug;
 const DEFAULT_VISIBILITY = LogVisibility.Developer;
 const MAX_LINE_LENGTH = parseInt(process.env.LOG_MAX_MESSAGE_LENGTH || '4096', 10);
 
