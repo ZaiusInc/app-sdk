10c419a20873def071db7f1e3855822bb799182d
Vickrum Loi <vic.loi@gmail.com>
2023-08-03 10:19:13 +0100

    OCP-482: Bump version.

--- src/queues/BaseQueue.ts
+++ src/queues/BaseQueue.ts
@@ -1,6 +1,6 @@
 export type JSONEncodable = string | number | boolean | null | JSONEncodable[] | { [key: string]: JSONEncodable };
 
 export interface BaseQueue {
-  send<T extends JSONEncodable>(queueName: string, message: T, group?: string): void;
-  sendBatch<T extends JSONEncodable>(queueName: string, messages: T[], group?: string): void;
+  send<T extends JSONEncodable>(queueName: string, message: T, group?: string): Promise<void>;
+  sendBatch<T extends JSONEncodable>(queueName: string, messages: T[], group?: string): Promise<void>;
 }
