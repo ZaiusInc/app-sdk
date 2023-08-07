c24758872ccd9e687d9bb87da90ae3bcc4110c40
Vickrum Loi <vic.loi@gmail.com>
2023-08-07 12:05:26 +0100

    OCP-482: Missed some stringifying

--- src/queues/LocalQueue.ts
+++ src/queues/LocalQueue.ts
@@ -3,10 +3,10 @@ import { BaseQueue } from './BaseQueue';
 
 export class LocalQueue implements BaseQueue {
   public async send(queueName: string, message: string, group?: string) {
-    logger.debug(`Sending message to queue: ${queueName}, group: ${group}, message: ${JSON.stringify(message)}`);
+    logger.debug(`Sending message to queue: ${queueName}, group: ${group}, message: ${message}`);
   }
 
   public async sendBatch(queueName: string, messages: string[], group?: string) {
-    logger.debug(`Sending message to queue: ${queueName}, group: ${group}, messages: ${JSON.stringify(messages)}`);
+    logger.debug(`Sending message to queue: ${queueName}, group: ${group}, messages: ${messages.join(', ')}`);
   }
 }
