a22dd4fe23e0a658167cddfa1604d58c49907e3b
Mohamed El Gabbouch <mohamed.elgabbouch@optimizely.com>
2023-11-02 09:31:14 +0000

    OCP-595: fix code review

--- src/queues/Queue.ts
+++ src/queues/Queue.ts
@@ -11,7 +11,8 @@ export interface Queue {
    * Send a single message to a queue.
    * @param queueName Name of the queue, as defined in app.yml
    * @param message JSON-seralizable message to send
-   * @param deduplicationId deduplication id for fifo queues
+   * @param deduplicationId deduplication id for fifo queues. If a message with a particular deduplicationId is sent successfully, 
+   * any messages sent with the same deduplicationId are accepted successfully but aren't delivered during a 5-minute interval.
    */
   send<T extends JSONEncodable>(queueName: string, message: T, deduplicationId?: string): Promise<void>;
 
@@ -19,7 +20,8 @@ export interface Queue {
    * Sends a batch of messages to a queue.
    * @param queueName Name of the queue, as defined in app.yml
    * @param messages List of JSON-seralizable messages to send
-   * @param deduplicationId deduplication id for fifo queues
+   * @param deduplicationId deduplication id for fifo queues. If a message with a particular deduplicationId is sent successfully, 
+   * any messages sent with the same deduplicationId are accepted successfully but aren't delivered during a 5-minute interval.
    */
   sendBatch<T extends JSONEncodable>(queueName: string, messages: T[], deduplicationId?: string): Promise<void>;
 }
